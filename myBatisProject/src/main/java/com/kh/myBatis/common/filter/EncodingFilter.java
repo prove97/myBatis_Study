252c792cb11cfb15581c026f5a22612be131dc7f
prove97 <dsr102938@gmail.com>
2024-04-15 14:39:53 +0900

    filter 추가

---
+++ myBatisProject/src/main/java/com/kh/myBatis/common/filter/EncodingFilter.java
@@ -0,0 +1,53 @@
+package com.kh.myBatis.common.filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+/**
+ * Servlet Filter implementation class EncodingFilter
+ */
+@WebFilter("/*") //@WebFilter("/*") 모든 
+public class EncodingFilter implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public EncodingFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		//필터가 종료할떄 해줄 작업
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		//요청을 가로채거나
+		System.out.println("인코딩필터 통과");
+		//특정 조건에 따라서 다른 필터나 서블릿으로 요청을 전달
+		request.setCharacterEncoding("UTF-8");
+		
+		chain.doFilter(request, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// 필터 초기화작업
+		// TODO Auto-generated method stub
+	}
+
+}
